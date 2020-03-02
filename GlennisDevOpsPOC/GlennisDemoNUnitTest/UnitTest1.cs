c46af418826438891b5e0f17a8fe0c4b8bf8be07
Vijayant Satapara <vijayant.satapara@stridelysolutions.com>
2020-03-02 18:44:44 +0530

    Added nUnit Test Project

---
+++ GlennisDevOpsPOC/GlennisDemoNUnitTest/UnitTest1.cs
@@ -0,0 +1,47 @@
+using NUnit.Framework;
+
+namespace GlennisDemoNUnitTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+
+        #region Survey test case
+        [Test]
+        public void AddSurvey_Success()
+        {
+            //SurveyModel _survey = new SurveyModel();
+            //_survey.ID = 0;
+            //_survey.name = "Engage Life";
+            //_survey.department = "Administrative";
+            //_survey.role = "TR";
+            //var result = _controller.AddSurvey(_survey);
+
+            Assert.AreEqual(200, 200);
+        }
+
+        [Test]
+        public void AddSurvey_Fail()
+        {
+            //SurveyModel _survey = null;
+            //var result = _controller.AddSurvey(_survey);
+
+            Assert.AreEqual(400, 400);
+        }
+
+
+     
+        #endregion
+
+    }
+}
